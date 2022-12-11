56f05ac05da965a687fb2d1079ef80ebd33d5a79
Gavin Grant <gavingrant@gmail.com>
2022-12-11 12:39:33 -0800

    refactor: stripe webhook

--- src/pages/api/stripe-webhook.js
+++ src/pages/api/stripe-webhook.js
@@ -1,5 +1,8 @@
 const getRawBody = require("raw-body");
-const { updateCustomerByStripeCid, getCustomerByStripeCid } = require("./_db.js");
+const {
+  updateCustomerByStripeCid,
+  getCustomerByStripeCid,
+} = require("./_db.js");
 const stripe = require("./_stripe.js");
 
 // Disable next.js body parsing (stripe needs the raw body to validate the event)
@@ -23,23 +26,23 @@ export default async (req, res) => {
 
     console.log(`stripeEvent: ${stripeEvent.type}`);
 
+    // Get the object from stripeEvent
+    const object = stripeEvent.data.object;
+
+    // Get the customer object
+    const cust = await getCustomerByStripeCid(object.customer);
+
+    // Get the number of tokens puchased based on price
+    let addedTokens = 0;
+    if (object.amount_total === 1000) {
+      addedTokens = 5;
+    } else if (object.amount_total === 1500) {
+      addedTokens = 10;
+    } else if (object.amount_total === 2000) {
+      addedTokens = 20;
+    }
+
     if (stripeEvent.type === "checkout.session.completed") {
-      // Get the object from stripeEvent
-      const object = stripeEvent.data.object;
-
-      // Get the customer object
-      const cust = await getCustomerByStripeCid(object.customer);
-
-      // Get the number of tokens puchased based on price
-      let addedTokens = 0;
-      if (object.amount_total === 1000) {
-        addedTokens = 5;
-      } else if (object.amount_total === 1500) {
-        addedTokens = 10;
-      } else if (object.amount_total === 2000) {
-        addedTokens = 20;
-      }
-
       // Update tokens amount for the current customer
       await updateCustomerByStripeCid(object.customer, {
         tokens: cust.tokens + addedTokens,
