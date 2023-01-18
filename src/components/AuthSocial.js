677c9ac9137bc848b271f5a13a2bd173f1ca18f4
Gavin Grant <gavingrant@gmail.com>
2023-01-18 10:25:48 -0800

    refactor: better contrast for dark mode in Google OAuth

--- src/components/AuthSocial.js
+++ src/components/AuthSocial.js
@@ -3,46 +3,51 @@ import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import { makeStyles } from "@mui/styles";
 import { useAuth } from "util/auth";
-
-const useStyles = makeStyles((theme) => ({
-  button: {
-    position: "relative",
-    "&:not(:last-child)": {
-      marginBottom: theme.spacing(1),
-    },
-  },
-  icon: {
-    position: "absolute",
-    alignItems: "center",
-    display: "inline-flex",
-    justifyContent: "center",
-    width: "1.5em",
-    height: "1.5em",
-    left: "0.5em",
-    "& > img": {
-      display: "block",
-      width: "100%",
-    },
-  },
-  lastUsedIndicator: {
-    position: "absolute",
-    top: "-6px",
-    right: "-6px",
-    fontSize: "13px",
-    lineHeight: 1,
-    textTransform: "initial",
-    padding: "3px 5px",
-    borderRadius: "7px",
-    color: "white",
-    backgroundColor: theme.palette.warning.main,
-    opacity: 0.8,
-  },
-}));
+import { useDarkMode } from "util/theme";
 
 function AuthSocial(props) {
-  const classes = useStyles();
+  const darkMode = useDarkMode();
+  const useStyles = makeStyles((theme) => ({
+    button: {
+      position: "relative",
+      "&:not(:last-child)": {
+        marginBottom: theme.spacing(1),
+      },
+    },
+    icon: {
+      position: "absolute",
+      alignItems: "center",
+      display: "inline-flex",
+      justifyContent: "center",
+      width: "1.5em",
+      height: "1.5em",
+      left: "0.5em",
+      "& > img": {
+        display: "block",
+        width: "100%",
+      },
+    },
+    lastUsedIndicator: {
+      position: "absolute",
+      top: "-6px",
+      right: "-6px",
+      fontSize: "13px",
+      lineHeight: 1,
+      textTransform: "initial",
+      padding: "3px 5px",
+      borderRadius: "7px",
+      color: "white",
+      backgroundColor: theme.palette.warning.main,
+      opacity: 0.8,
+    },
+    text: {
+      color: darkMode.value ? "white" : "black",
+    },
+  }));
 
+  const classes = useStyles();
   const auth = useAuth();
+
   const [pending, setPending] = useState(null);
   const [lastUsed, setLastUsed] = useState(null);
 
@@ -97,7 +102,7 @@ function AuthSocial(props) {
           </div>
 
           {pending !== provider && (
-            <span>
+            <span className={classes.text}>
               {props.buttonAction} with {providerDisplayNames[provider]}
             </span>
           )}
