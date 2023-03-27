653bb808f2fcac83fa34fb0c3f4c2c5649fc7905
Gavin Grant <gavingrant@gmail.com>
2023-03-26 18:14:27 -0700

    feat: add examples page

---
+++ src/components/ExamplesSection.js
@@ -0,0 +1,212 @@
+import React from "react";
+import Link from "next/link";
+import Container from "@mui/material/Container";
+import LongContent from "components/LongContent";
+import CtaSection from "./CtaSection";
+
+function ExamplesSection(props) {
+  const CallToAction = () => (
+    <CtaSection
+      bgColor="primary"
+      size="medium"
+      bgImage=""
+      bgImageOpacity={1}
+      title="Let us write your description for you!"
+      subtitle="Listingify can write your description in seconds starting at $1 per description."
+      buttonText="Sign Up"
+      buttonColor="secondary"
+      buttonPath="/auth/signup"
+    />
+  );
+
+  return (
+    <LongContent>
+      <Container>
+        <h1>Real Estate Listing Description Examples</h1>
+        <p>
+          Are you struggling to come up with the perfect words to describe your
+          real estate listing? Writing a compelling listing description is
+          crucial to attract potential buyers and showcase the unique features
+          of your property. It can be challenging to find the right words to
+          make your listing stand out in a sea of others. That's why we've
+          compiled a list of real estate listing description examples to help
+          inspire you and create a powerful listing that captures the attention
+          of potential buyers. Whether you're a seasoned real estate agent or a
+          first-time seller, these examples will help you write a description
+          that highlights the best features of your property and entices buyers
+          to schedule a showing.
+        </p>
+        <p>
+          What's unique about this list of examples is that they are all written
+          by our proprietary, virtual real estate listing description
+          copywriting service <Link href="/">Listingify</Link>. Before we get
+          into the examples, let's discuss what makes a great real estate
+          listing description.
+        </p>
+
+        <CallToAction />
+
+        <h2>Writing a Real Estate Listing Description</h2>
+        <p>
+          When it comes to selling a property, the listing description can make
+          all the difference. A well-written description can attract more
+          potential buyers and lead to a quicker sale. Here are some tips for
+          writing a great real estate listing description:
+        </p>
+        <h3>1. Provide detailed information about the property</h3>
+        <p>
+          In your description, be sure to include all the important details
+          about the property, such as:{" "}
+          <ul>
+            <li>Number of bedrooms and bathrooms</li>
+            <li>Square footage</li>
+            <li>Lot size Age of the property</li>
+            <li>Special features, such as a pool or fireplace</li>
+            <li>Any recent upgrades or renovations</li>
+          </ul>{" "}
+          Provide as much information as possible to help potential buyers get a
+          good sense of the property before they see it in person.
+        </p>
+        <h3>2. Use descriptive language</h3>
+        <p>
+          When writing your description, use descriptive language to paint a
+          picture of the property. Instead of saying "spacious living room," try
+          "bright and airy living room with soaring ceilings." Use words that
+          will help potential buyers envision themselves living in the space.
+        </p>
+        <h3>3. Highlight the property's best features</h3>
+        <p>
+          Make sure to highlight the property's best features, whether it's a
+          stunning view or a gourmet kitchen. Be sure to mention these features
+          early in your description to grab the reader's attention.
+        </p>
+        <h3>4. Provide information about the neighborhood</h3>
+        <p>
+          Don't forget to include information about the neighborhood in your
+          description. Mention nearby amenities, such as parks, restaurants, and
+          schools. This can help potential buyers get a sense of what it would
+          be like to live in the area.
+        </p>
+        <h3>5. End with a strong call to action</h3>
+        <p>
+          Finally, end your description with a strong call to action, such as
+          "Schedule a showing today!" This encourages potential buyers to take
+          action and see the property in person.
+        </p>
+
+        <CallToAction />
+
+        <h2>Real Estate Listing Description Example #1</h2>
+        <p>
+          Looking for a spacious and beautiful single-family home in the Baldwin
+          Park neighborhood of Orlando, FL? Look no further than 2752 Stanfield
+          Ave! This stunning property boasts 5 bedrooms and 4.5 baths, and is
+          perfect for families or those who love to entertain guests. With 3722
+          square feet of living space on a 6653 square foot lot, this property
+          offers an abundance of space and privacy.
+        </p>
+        <p>
+          One of the standout features of this property is its beautiful and
+          well-appointed interior. The central air conditioning and central heat
+          will keep you comfortable no matter the temperature outside, while the
+          hardwood flooring and marble countertops in the kitchen give the home
+          a luxurious and sophisticated feel. Additional interior features
+          include stainless steel appliances, shaker cabinets, a dining room,
+          laundry room, walk-in kitchen pantry, and ceiling fans throughout. The
+          primary bath also boasts a separate tub and shower, as well as a
+          double vanity, while a bonus room provides additional space for a home
+          office, playroom, or whatever your heart desires.
+        </p>
+        <p>
+          Living in Baldwin Park is a dream come true for many residents of
+          Orlando, and it's easy to see why. With its beautiful tree-lined
+          streets, ample sidewalks and bike paths, and close proximity to some
+          of the city's best amenities, there's truly something for everyone
+          here. Notable landmarks include Lake Baldwin, which is perfect for
+          picnics, bike rides, and other outdoor activities, as well as Winter
+          Park Village, a popular shopping and dining destination. With a high
+          walkability score and a tight-knit community feel, Baldwin Park truly
+          is the perfect place to call home. Don't miss your chance to see this
+          stunning property in person – schedule a tour today!
+        </p>
+
+        <h2>Real Estate Listing Description Example #2</h2>
+        <p>
+          Welcome to your new home in the heart of Chicago's Fulton Market
+          neighborhood! This stunning 2-bedroom, 2-bathroom condo located at 550
+          W Fulton St #203 is a must-see for anyone looking for contemporary
+          design, city skyline views, and an unbeatable location. With a
+          spacious 1350 sf floor plan and plenty of natural light, this condo is
+          perfect for entertaining guests or relaxing after a long day. Plus,
+          with 2 parking garage spaces included, you won't have to worry about
+          finding a place to park in this bustling neighborhood.
+        </p>
+        <p>
+          Step inside and discover all the amazing interior features this condo
+          has to offer. The gas fireplace, central air conditioning, and central
+          heat will keep you comfortable year-round, while the engineered wood
+          flooring and open floor plan create a warm and inviting atmosphere.
+          The farmhouse sink, quartz countertops, shaker cabinets, and stainless
+          steel appliances make the kitchen a chef's dream, while the smart
+          thermostat and walk-in closet in the primary bedroom add a touch of
+          luxury. Plus, with a tub and shower in one bathroom and a separate tub
+          and shower in the other, everyone can get ready for the day in peace.
+        </p>
+        <p>
+          But it's not just the interior that makes this condo so special. The
+          brick finish and dual pane windows give the exterior a sleek and
+          modern look, while the balcony allows you to take in the stunning city
+          skyline views. Plus, with plenty of shops and restaurants nearby, this
+          walkable community is perfect for anyone who loves to explore. And if
+          you're a fan of farmhouse style, you'll love the unique feature of a
+          farmhouse sink in the kitchen.
+        </p>
+        <p>
+          Living in the Fulton Market neighborhood of Chicago means you'll be
+          surrounded by some of the city's most notable landmarks and
+          highlights. From the trendy restaurants and bars on Randolph Street to
+          the vibrant street art and murals that cover the walls, there is
+          always something new to discover. Plus, with easy access to public
+          transportation and major highways, you'll be able to explore all that
+          Chicago has to offer. Don't miss your chance to see this amazing condo
+          before it's gone. Schedule a tour today!
+        </p>
+
+        <h2>Real Estate Listing Description Example #3</h2>
+        <p>
+          This stunning townhouse at 1329 California Ave #101 is the epitome of
+          modern luxury living. With two bedrooms, two-and-a-half baths, and a
+          spacious floor area of 2274 square feet, this new construction is
+          turn-key and truly move-in ready. The attached two-car garage makes
+          coming home a breeze, while the high ceilings and gas fireplace create
+          a warm and inviting atmosphere. Built in 2020, this townhouse boasts a
+          bonus room, home office, laundry room, and even a skylight to let in
+          natural light.
+        </p>
+        <p>
+          One of the standout features of this property is the skylight that
+          floods the space with natural light. Imagine waking up every morning
+          to the sun streaming in, or falling asleep to the stars above. With
+          the skylight, you can enjoy the benefits of natural light without
+          sacrificing privacy. In addition, the skylight adds an extra element
+          of sophistication to this already luxurious townhouse. This unique
+          feature truly sets this property apart from the rest.
+        </p>
+        <p>
+          Located in the heart of Santa Monica, this property is in close
+          proximity to all of the city's notable landmarks and attractions. From
+          the iconic Santa Monica Pier to the world-famous Third Street
+          Promenade, there is always something to see and do in this vibrant
+          community. With a walkability score of 82, residents can easily
+          explore the local restaurants, cafes, and shops that make Santa Monica
+          such a desirable place to live. Don't miss your chance to tour this
+          one-of-a-kind property - schedule a viewing today!
+        </p>
+
+        <CallToAction />
+      </Container>
+    </LongContent>
+  );
+}
+
+export default ExamplesSection;
---
+++ src/pages/real-estate-listing-description-examples.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Meta from "components/Meta";
+import ExamplesSection from "components/ExamplesSection";
+
+function Examples() {
+  return (
+    <>
+      <Meta
+        title="Real estate listing description examples"
+        description="See examples of well-written real estate listing descriptions."
+      />
+      <ExamplesSection />
+    </>
+  );
+}
+
+export default Examples;
